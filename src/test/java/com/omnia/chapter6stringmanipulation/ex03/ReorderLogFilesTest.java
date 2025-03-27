ddd2556e2bea124da5a8f3c005c2f06a588d4936
Jong-Dae Park <jongdae837@gmail.com>
2025-03-27 22:17:34 +0900

    test: 시간 측정 코드 공통화

--- src/test/java/com/omnia/chapter6stringmanipulation/ex03/ReorderLogFilesTest.java
+++ src/test/java/com/omnia/chapter6stringmanipulation/ex03/ReorderLogFilesTest.java
@@ -1,10 +1,9 @@
 package com.omnia.chapter6stringmanipulation.ex03;
 
+import com.omnia.chapter6stringmanipulation.MeasureTimeUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
-import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,39 +16,22 @@ class ReorderLogFilesTest {
 
     @Test
     void test() {
-        Instant startTime = Instant.now();
-
-        String[] reversed = ReorderLogFiles.reorderLogFiles(INPUT);
-
-        Instant endTime = Instant.now();
+        String[] reversed = MeasureTimeUtils.measureExecutionTime("ReorderLogFilesTest#test", () -> ReorderLogFiles.reorderLogFiles(INPUT));
 
         Assertions.assertTrue(Arrays.equals(reversed, OUTPUT));
-
-        System.out.println("ReorderLogFilesTest#test: " + Duration.between(startTime, endTime).toNanos() + " ns");
     }
 
     @Test
     void test_using_list_input_and_output() {
-        Instant startTime = Instant.now();
-
-        List<String> reversed = ReorderLogFiles.reorderLogFilesUsingList(INPUT_LIST);
-
-        Instant endTime = Instant.now();
+        List<String> reversed = MeasureTimeUtils.measureExecutionTime("ReorderLogFilesTest#test_using_list_input_and_output", () -> ReorderLogFiles.reorderLogFilesUsingList(INPUT_LIST));
 
         Assertions.assertTrue(reversed.equals(OUTPUT_LIST));
-
-        System.out.println("ReorderLogFilesTest#test_using_list_input_and_output: " + Duration.between(startTime, endTime).toNanos() + " ns");
     }
 
     @Test
     void testAnswer() {
-        Instant startTime = Instant.now();
+        String[] reversed = MeasureTimeUtils.measureExecutionTime("ReorderLogFilesTest#testAnswer", () -> ReorderLogFiles.reorderLogFilesAnswer(INPUT));
 
-        String[] reversed = ReorderLogFiles.reorderLogFilesAnswer(INPUT);
         Assertions.assertTrue(Arrays.equals(reversed, OUTPUT));
-
-        Instant endTime = Instant.now();
-
-        System.out.println("ReorderLogFilesTest#testAnswer: " + Duration.between(startTime, endTime).toNanos() + " ns");
     }
 }
