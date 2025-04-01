3d0c464bbaf2eff9f1b12caf81f7fe39b123f8f0
Jong-Dae Park <jongdae837@gmail.com>
2025-04-01 21:49:21 +0900

    feat: 가장 긴 팰린드롬 부분 문자열 추가

---
+++ src/test/java/com/omnia/chapter6stringmanipulation/ex06/LongestPalindromeSubstringTest.java
@@ -0,0 +1,20 @@
+package com.omnia.chapter6stringmanipulation.ex06;
+
+import com.omnia.chapter6stringmanipulation.MeasureTimeUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LongestPalindromeSubstringTest {
+
+    private static final String INPUT = "dcbabcdd";
+    private static final String OUTPUT = "dcbabcd";
+
+    @Test
+    void testAnswer() {
+        LongestPalindromeSubstring longest = new LongestPalindromeSubstring();
+        String result = MeasureTimeUtils.measureExecutionTime("GroupAnagramsTest#testAnswer", () -> longest.longestPalindromeAnswer(INPUT));
+
+        assertEquals(OUTPUT, result);
+    }
+}
