0410650ddf2ad98893414ecdab01df6a4dd5ce4c
Jong-Dae Park <jongdae837@gmail.com>
2025-04-12 22:38:17 +0900

    feat: 두 수의 합 추가

---
+++ src/main/java/com/omnia/chapter7Array/ex07/TwoSum.java
@@ -0,0 +1,48 @@
+package com.omnia.chapter7Array.ex07;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        List<ElementWithIndex> elements = IntStream.range(0, nums.length)
+                .mapToObj(i -> new ElementWithIndex(nums[i], i))
+                .collect(Collectors.toList());
+
+        elements.sort(Comparator.comparingInt(ElementWithIndex::value));
+
+        while (left < right) {
+            if (elements.get(left).value() + elements.get(right).value() < target) {
+                left++;
+            } else if (elements.get(left).value() + elements.get(right).value() > target) {
+                right--;
+            } else {
+                return new int[]{elements.get(left).index(), elements.get(right).index()};
+            }
+        }
+        return null;
+    }
+
+    public static int[] twoSumAnswer(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
+
+record ElementWithIndex(int value, int index) {
+}
