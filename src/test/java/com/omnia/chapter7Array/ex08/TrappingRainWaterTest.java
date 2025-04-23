947d28d03154483ee2de028467cfa59637b29cf1
Jong-Dae Park <jongdae837@gmail.com>
2025-04-23 22:54:53 +0900

    feat: 빗물 트래핑 추가

---
+++ src/test/java/com/omnia/chapter7Array/ex08/TrappingRainWaterTest.java
@@ -0,0 +1,25 @@
+package com.omnia.chapter7Array.ex08;
+
+import com.omnia.chapter6stringmanipulation.MeasureTimeUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TrappingRainWaterTest {
+
+    private static final int[] INPUT = {1, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+    private static final int OUTPUT = 6;
+
+    @Test
+    void testAnswer() {
+        int result = MeasureTimeUtils.measureExecutionTime("TrappingRainWaterTest#testAnswer", () -> TrappingRainWater.trapAnswer( INPUT));
+
+        Assertions.assertEquals(result, OUTPUT);
+    }
+
+    @Test
+    void testAnswerWithStack() {
+        int result = MeasureTimeUtils.measureExecutionTime("TrappingRainWaterTest#testAnswerWithStack", () -> TrappingRainWater.trapAnswerWithStack(INPUT));
+
+        Assertions.assertEquals(result, OUTPUT);
+    }
+}
